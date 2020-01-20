8b1874b0608db7f88e1ffed24ad545157ea5c6e8
Todd Kennedy <todd@selfassembled.org>
2020-01-20 12:31:55 -0800

    huh

--- release.sh
+++ release.sh
@@ -18,15 +18,15 @@ render_markdown () {
   echo "Rendering markdown"
   curl -sX POST https://api.github.com/markdown -d "{\"text\": \"$(cat README.md | perl -pe 's/\n/\\n/g' | perl -pe 's/\"/\\"/g')\"}" > index.html
   echo "Uploading markdown to ${SERVER}/${UPLOAD_PATH}/${PROJECT}/"
-  curl -u ${USER}:${TOKEN} -T index.html ${SERVER}/${UPLOAD_PATH}/${PROJECT}/
-  curl -u ${USER}:${TOKEN} -T index.html ${SERVER}/${UPLOAD_PATH}/${PROJECT}/latest/
-  curl -u ${USER}:${TOKEN} -T index.html ${SERVER}/${UPLOAD_PATH}/${PROJECT}/${VERSION}/
+  curl -u ${USER}:${TOKEN} -sT index.html ${SERVER}/${UPLOAD_PATH}/${PROJECT}/
+  curl -u ${USER}:${TOKEN} -sT index.html ${SERVER}/${UPLOAD_PATH}/${PROJECT}/latest/
+  curl -u ${USER}:${TOKEN} -sT index.html ${SERVER}/${UPLOAD_PATH}/${PROJECT}/${VERSION}/
 }
 
 create_release () {
   echo "Marking release ${VERSION} on GitHub"
   echo "{\"tag_name\":\"v${VERSION}\",\"target_commitish\":\"master\",\"name\":\"v${VERSION}\",\"body\":\"Take-Five version ${VERSION}\"}" > release.json
-  curl -u ${GH_USER}:${GH_TOKEN} -X POST https://api.github.com/repos/toddself/take-five/releases -d @release.json
+  echo curl -u ${GH_USER}:${GH_TOKEN} -sX POST https://api.github.com/repos/toddself/take-five/releases -d @release.json
 }
 
 make_path $PROJECT
@@ -36,8 +36,8 @@ make_path $PROJECT/$VERSION
 for FILE in "$@"
 do
   echo "Uploading "${FILE}" to "${UPLOAD_PATH}/${PROJECT}
-  curl -u ${USER}:${TOKEN} -T $FILE ${SERVER}/${UPLOAD_PATH}/${PROJECT}/latest/
-  curl -u ${USER}:${TOKEN} -T $FILE ${SERVER}/${UPLOAD_PATH}/${PROJECT}/${VERSION}/
+  curl -u ${USER}:${TOKEN} -sT $FILE ${SERVER}/${UPLOAD_PATH}/${PROJECT}/latest/
+  curl -u ${USER}:${TOKEN} -sT $FILE ${SERVER}/${UPLOAD_PATH}/${PROJECT}/${VERSION}/
 done
 
 render_markdown
